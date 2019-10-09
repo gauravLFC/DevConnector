7153535ecf0600f899a98b0c8d786cf7593fd625
Gaurav Sharma <gauravlfc.93@gmail.com>
2019-10-09 21:15:26 +0530

    Added backend apis

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const connectDB = require('./config/db');
+
+//Connect Database
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/apis/users'));
+app.use('/api/posts', require('./routes/apis/post'));
+app.use('/api/auth', require('./routes/apis/auth'));
+app.use('/api/profile', require('./routes/apis/profile'));
 
 const PORT = process.env.PORT || 3000;
 
